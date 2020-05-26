f366a88d605bfdd55b58dedc47be5eb572bb8f5d
unknown <it.masdelima@gmail.com>
2020-05-26 18:26:20 +0700

    Reset Password

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -24,6 +24,14 @@ Class Account Extends REST_Controller{
         
         if($this->form_validation->run() === true) {
             $res = $this->Mod_account->register();
+            $newdata = array(
+                        'Id'  	        => $res['Id'],	            
+                        'Email'         => $res['Email'],
+                        'LastName'      => $res['LastName'],
+                        'FirstName'     => $res['FirstName']
+                    );
+
+            $user = $this->session->set_userdata($newdata);
             if($res == TRUE){
                 $this->response([
                     'result'  => TRUE,
@@ -34,6 +42,47 @@ Class Account Extends REST_Controller{
                 $this->response([
                     'result'  => FALSE,
                     'message' => 'Cannot Registered your Account'
+                ], REST_Controller::HTTP_OK);
+            }
+        }
+        else {
+            $this->response([
+                'result'  => FALSE,
+                'message' => $this->form_validation->error_array()
+            ], REST_Controller::HTTP_OK);
+        }
+    }
+
+    Public function verify_otp_post(){
+        $Id  = $this->session->userdata('Id');
+        $Otp = $this->input->post('otp');
+        $validator = array('success' => false, 'messages' => array());
+        $this->form_validation->set_rules('otp', 'OTP', 'required');
+
+        $this->form_validation->set_message('required', 'You must enter a %s.');
+        $this->form_validation->set_message('is_unique', 'That %s is already in use, please try again.');
+        
+        if($this->form_validation->run() === true) {
+            $validator = array('success' => false, 'messages' => array());
+            $this->db->select('Id, otp');
+            $this->db->from('otp'); 
+            $this->db->where('PatientId', $Id);
+            $this->db->where('OTP', $Otp);
+            $this->db->where('RecordStatus', 1);
+            $this->db->where('ExpiredTime >', date('Y-m-d H:i:s',now()));
+            $result = $this->db->get();
+            
+            if($result->num_rows()) {
+                $this->db->query("UPDATE OTP SET RecordStatus = 0 WHERE PatientId=$Id AND OTP=$Otp");
+                $this->response([
+                    'result'  => TRUE,
+                    'message' => 'Success'
+                ], REST_Controller::HTTP_OK);
+            }
+            else {
+                $this->response([
+                    'result'  => FALSE,
+                    'message' => 'Otp not Match'
                 ], REST_Controller::HTTP_OK);
             }
         }
@@ -46,80 +95,31 @@ Class Account Extends REST_Controller{
     }
 
     public function checkUname_post(){
-        $Username = $this->input->post('Username');
         $validator = array('success' => false, 'messages' => array());
-        $this->db->select('Id, FirstName, Lastname');
-        $this->db->from('patient'); 
-        $this->db->where('Email', $Username);
-        $this->db->or_where('MobilePhone', $Username);
-        $result = $this->db->get();
+        $this->form_validation->set_rules('Username', 'Username', 'required');
 
-        if($result->num_rows()) {
-            $this->response([
-                'result'  => TRUE,
-                'message' => 'User Found'
-            ], REST_Controller::HTTP_OK);
-        }
-        else {
-            $this->response([
-                'result'  => FALSE,
-                'message' => 'User Not Found'
-            ], REST_Controller::HTTP_OK);
-        }
-    }
+        $this->form_validation->set_message('required', 'You must enter a %s.');
+        $this->form_validation->set_message('is_unique', 'That %s is already in use, please try again.');
 
-    public function login_post(){
-        $login = $this->Mod_account->login();
-        if($login) {
-            $this->load->library('session');
+        if($this->form_validation->run() === true) {
+            $res = $this->Mod_account->checkUname();
+
             $newdata = array(
-                'id'  	        => $login['Id'],	      
-                'sessionid'  	=> $this->session->userdata('session_id'),	      
-                'firstname'     => $login['FirstName'],
-                'lastname'      => $login['LastName']
+                'Id'  	        => $res['Id']
             );
 
             $user = $this->session->set_userdata($newdata);
-            $this->response([
-                'result'  => TRUE,
-                'message' => 'User login successfull.',
-            ], REST_Controller::HTTP_OK);				
-        }
-        else {
-            $this->response([
-                'result'  => FALSE,
-                'message' => 'Wrong Address or Password'
-            ], REST_Controller::HTTP_OK);
-        } 
-    }
-    
-    public function signup_post(){
-        $validator = array('success' => false, 'messages' => array());
-        $this->form_validation->set_rules('UserName', 'Username', 'required|is_unique[SysMembership.UserName]');
-        $this->form_validation->set_rules('Password', 'Password', 'required');
-        $this->form_validation->set_rules('FirstName', 'FirstName', 'required');
-        $this->form_validation->set_rules('LastName', 'LastName', 'required');
-        $this->form_validation->set_rules('GenderId', 'Gender', 'required');
-        $this->form_validation->set_rules('DateOfBirth', 'DOB', 'required');
-        $this->form_validation->set_rules('MobilePhone', 'Phone', 'required');
-        $this->form_validation->set_rules('Email', 'Email', 'required');
 
-        /* Custom Messages */
-        $this->form_validation->set_message('required', 'You must enter a %s.');
-		$this->form_validation->set_message('is_unique', 'That %s is already in use, please try again.');	
-        
-        if($this->form_validation->run() === true) {
-            $res = $this->Mod_account->signup();
-            if($res == TRUE){
+            if($res == TRUE) {
                 $this->response([
                     'result'  => TRUE,
-                    'message' => 'A OTP has been sent to your email address'
+                    'message' => 'User Found'
                 ], REST_Controller::HTTP_OK);
             }
-            else{
+            else {
                 $this->response([
                     'result'  => FALSE,
-                    'message' => 'Cannot Registered your Account'
+                    'message' => 'User Not Found'
                 ], REST_Controller::HTTP_OK);
             }
         }
@@ -130,6 +130,111 @@ Class Account Extends REST_Controller{
             ], REST_Controller::HTTP_OK);
         }
     }
+
+    public function login_post(){
+        $login = $this->Mod_account->login();
+        if($login) {
+            $this->load->library('session');
+            $newdata = array(
+                        'Id'  	        => $login['Id'],	            
+                        'Email'         => $login['Email'],
+                        'LastName'      => $login['LastName'],
+                        'FirstName'     => $login['FirstName']
+                    );
+
+            $this->session->set_userdata($newdata);
+            $this->response([
+                'result'  => True,
+                'message' => 'User login successfull.',
+            ], REST_Controller::HTTP_OK);				
+        }
+        else {
+            $this->response([
+                'result'  => FALSE,
+                'message' => 'Wrong Address or Password'
+            ], REST_Controller::HTTP_OK);
+        } 
+    }
+
+    public function resetPassword_post(){
+        $validator = array('success' => false, 'messages' => array());
+        $this->form_validation->set_rules('Password', 'Password', 'required');
+        $this->form_validation->set_rules('Repassword', 'Confirm Password', 'required|matches[Password]');
+
+        $this->form_validation->set_message('required', 'You must enter a %s.');
+        $this->form_validation->set_message('is_unique', 'That %s is already in use, please try again.');
+
+        if($this->form_validation->run() === true) {
+            $res = $this->Mod_account->resetPassword();
+
+            if($res == TRUE) {
+                $this->response([
+                    'result'  => TRUE,
+                    'message' => 'Success'
+                ], REST_Controller::HTTP_OK);
+            }
+            else {
+                $this->response([
+                    'result'  => FALSE,
+                    'message' => 'Failed!!'
+                ], REST_Controller::HTTP_OK);
+            }
+        }
+        else {
+            $this->response([
+                'result'  => FALSE,
+                'message' => $this->form_validation->error_array()
+            ], REST_Controller::HTTP_OK);
+        }
+    }
+
+    public function logout_get(){
+        foreach ($_SESSION as $key => $value) {
+          unset($_SESSION[$key]);
+        }
+        $this->response([
+            'result'  => True,
+            'message' => 'User logout successfull.',
+        ], REST_Controller::HTTP_OK);
+      }
+    
+    // public function signup_post(){
+    //     $validator = array('success' => false, 'messages' => array());
+    //     $this->form_validation->set_rules('UserName', 'Username', 'required|is_unique[SysMembership.UserName]');
+    //     $this->form_validation->set_rules('Password', 'Password', 'required');
+    //     $this->form_validation->set_rules('FirstName', 'FirstName', 'required');
+    //     $this->form_validation->set_rules('LastName', 'LastName', 'required');
+    //     $this->form_validation->set_rules('GenderId', 'Gender', 'required');
+    //     $this->form_validation->set_rules('DateOfBirth', 'DOB', 'required');
+    //     $this->form_validation->set_rules('MobilePhone', 'Phone', 'required');
+    //     $this->form_validation->set_rules('Email', 'Email', 'required');
+
+    //     /* Custom Messages */
+    //     $this->form_validation->set_message('required', 'You must enter a %s.');
+	// 	$this->form_validation->set_message('is_unique', 'That %s is already in use, please try again.');	
+        
+    //     if($this->form_validation->run() === true) {
+    //         $res = $this->Mod_account->signup();
+    //         if($res == TRUE){
+    //             $this->response([
+    //                 'result'  => TRUE,
+    //                 'message' => 'A OTP has been sent to your email address'
+    //             ], REST_Controller::HTTP_OK);
+    //         }
+    //         else{
+    //             $this->response([
+    //                 'result'  => FALSE,
+    //                 'message' => 'Cannot Registered your Account'
+    //             ], REST_Controller::HTTP_OK);
+    //         }
+    //     }
+    //     else {
+    //         $this->response([
+    //             'result'  => FALSE,
+    //             'message' => $this->form_validation->error_array()
+    //         ], REST_Controller::HTTP_OK);
+    //     }
+    // }
     //end proses signup
 
 }
--- application/models/Mod_account.php
+++ application/models/Mod_account.php
@@ -14,7 +14,7 @@ class Mod_account extends CI_Model {
 			$mobile = $this->input->post('Email');
 		} elseif(filter_var($email, FILTER_VALIDATE_EMAIL)) {
 			$mail = $this->input->post('Email');
-			$this->send_otp($otpkey,$email);
+			$this->mail_otp($otpkey,$email);
 		}
 		$this->db->trans_start();
 		
@@ -28,7 +28,7 @@ class Mod_account extends CI_Model {
 			'Email'			=> $mail,
 			'RecordStatus'  => 1,
 			'CreatedDate'	=> date('Y-m-d H:i:s',now()),
-			'CreatedBy'		=> 1
+			'CreatedBy'		=> $this->input->post('FirstName').' '.$this->input->post('LastName')
 		);
 		$this->db->insert('Patient', $patient);
 		$PatientId = $this->db->insert_id();
@@ -39,14 +39,18 @@ class Mod_account extends CI_Model {
 			'ExpiredTime'	=> date("Y-m-d H:i:s",strtotime(date("Y-m-d H:i:s")." +3 minutes")),
 			'RecordStatus'  => 1,
 			'CreatedDate'	=> date('Y-m-d H:i:s',now()),
-			'CreatedBy'		=> 1
+			'CreatedBy'		=> $this->input->post('FirstName').' '.$this->input->post('LastName')
 		);
 		$this->db->insert('OTP', $otp);
 
 		$afftectedRows=$this->db->affected_rows();
-        $this->db->trans_complete();
+		$this->db->trans_complete();
+		$this->db->select('Id, FirstName, LastName, Password, Email');
+		$this->db->from('patient'); 
+        $this->db->where('Id', $PatientId);
+		$user = $this->db->get()->row_array();
         if($afftectedRows>0){
-            return TRUE;
+            return $user;
         }
         else{
             return FALSE;
@@ -56,82 +60,50 @@ class Mod_account extends CI_Model {
 	public function login(){
         $Username = $this->input->post('Username');
 		$Password = $this->input->post('Password');
+		$otpkey = mt_rand(1000,9999);
+		$mobile = NULL;
+		$mail = NULL;
 
-		$this->db->select('Id, FirstName, LastName, Password');
+		$this->db->select('Id, FirstName, LastName, Password, Email');
 		$this->db->from('patient'); 
         $this->db->where('Email', $Username)
                 ->or_where('MobilePhone', $Username);
-		$user = $this->db->get()->row();
+		$user = $this->db->get()->row_array();
 		
 		if($user){
-			// periksa password-nya
-			$isPasswordTrue = password_verify($Password, $user->Password);
+			$isPasswordTrue = password_verify($Password, $user['Password']);
 
 			if($isPasswordTrue){ 
-                return true;
+				if (filter_var($Username, FILTER_SANITIZE_NUMBER_INT)) {
+					$mobile = $this->input->post('Email');
+				} elseif(filter_var($Username, FILTER_VALIDATE_EMAIL)) {
+					$this->mail_otp($otpkey,$user['Email']);
+				}
+				$this->db->trans_start();
+                $otp = array(
+					'PatientId'		=> $user['Id'],
+					'OTP'			=> $otpkey,
+					'ExpiredTime'	=> date("Y-m-d H:i:s",strtotime(date("Y-m-d H:i:s")." +3 minutes")),
+					'RecordStatus'  => 1,
+					'CreatedDate'	=> date('Y-m-d H:i:s',now()),
+					'CreatedBy'		=> 1
+				);
+				$this->db->insert('OTP', $otp);
+		
+				$afftectedRows=$this->db->affected_rows();
+				$this->db->trans_complete();
+				if($afftectedRows>0){
+					return $user;
+				}
+				else{
+					return FALSE;
+				}
             }
 		}
 		return false;  
 	}
-
-    public function signup(){
-		$salt = $this->salt();
-		$otpkey = mt_rand(1000,9999); 
-		$password = $this->makePassword($this->input->post('Password'), $salt);
-		$email = $this->input->post('Email');
-		
-		$this->db->trans_start();
-		
-		$member   = array(
-            'UserName'    	 => $this->input->post('UserName'),
-			'Password'     	 => $password,
-			'HospitalUnitId' => 1,
-			'RecordStatus'   => 1,
-			'CreatedDate'	 => date('Y-m-d H:i:s',now()),
-			'CreatedBy'		 => 1
-		);
-		$this->db->insert('SysMembership', $member);
-		$MembershipId = $this->db->insert_id();
-		
-		$patient = array(
-			'FirstName'     => $this->input->post('FirstName'),
-			'LastName'		=> $this->input->post('LastName'),
-			'Password'     	=> $password,
-			'GenderId'		=> $this->input->post('GenderId'),
-			'DateOfBirth'   => $this->input->post('DateOfBirth'),
-			'MobilePhone'	=> $this->input->post('MobilePhone'),
-			'Email'			=> $this->input->post('Email'),
-			'RecordStatus'  => 1,
-			'CreatedDate'	=> date('Y-m-d H:i:s',now()),
-			'CreatedBy'		=> 1
-		);
-		$this->db->insert('Patient', $patient);
-		$PatientId = $this->db->insert_id();
-
-		$otp = array(
-			'PatientId'		=> $PatientId,
-			'MembershipId'	=> $MembershipId,
-			'OTP'			=> $otpkey,
-			'ExpiredTime'	=> date("Y-m-d H:i:s",strtotime(date("Y-m-d H:i:s")." +2 minutes")),
-			'RecordStatus'  => 1,
-			'CreatedDate'	=> date('Y-m-d H:i:s',now()),
-			'CreatedBy'		=> 1
-		);
-		$this->db->insert('OTP', $otp);
-
-		$this->send_otp($otpkey,$email);
-
-		$afftectedRows=$this->db->affected_rows();
-        $this->db->trans_complete();
-        if($afftectedRows>0){
-            return TRUE;
-        }
-        else{
-            return FALSE;
-        }
-	}
 	
-	function send_otp($otp,$email){
+	function mail_otp($otp,$email){
         $from_email = "no-reply@masdelima.net"; 
         $to_email = $email; 
 
@@ -142,7 +114,7 @@ class Mod_account extends CI_Model {
             'smtp_user' => 'no-reply@masdelima.net',
             'smtp_pass' => 'Bogor@16',
             'mailtype'  => 'html',
-            'charset'  => 'iso-8859-1',
+            'charset'  	=> 'iso-8859-1',
             'wordwrap'  => TRUE
         );
 
@@ -154,6 +126,117 @@ class Mod_account extends CI_Model {
         $this->email->subject('Verify Your Register'); 
         $this->email->message('Code your OTP is '.$otp);
 		$this->email->send();
-    }
+	}
+	
+	function checkUname(){
+		$Username = $this->input->post('Username');
+		$otpkey = mt_rand(1000,9999);
+		$this->db->select('Id, FirstName, Lastname, Email');
+		$this->db->from('patient'); 
+		$this->db->where('Email', $Username);
+		$this->db->or_where('MobilePhone', $Username);
+		$result = $this->db->get();
+		$id = $result->row_array();
+		if($result->num_rows()){
+			$this->db->select('Id, FirstName, LastName, Password, Email');
+			$this->db->from('patient'); 
+			$this->db->where('Id', $id['Id']);
+			$a = $this->db->get();
+			$user = $a->row_array();
+			$this->mail_otp($otpkey,$id['Email']);
 
+			$otp = array(
+				'PatientId'		=> $id['Id'],
+				'OTP'			=> $otpkey,
+				'ExpiredTime'	=> date("Y-m-d H:i:s",strtotime(date("Y-m-d H:i:s")." +3 minutes")),
+				'RecordStatus'  => 1,
+				'CreatedDate'	=> date('Y-m-d H:i:s',now()),
+				'CreatedBy'		=> 1
+			);
+			$this->db->insert('OTP', $otp);
+
+			return $user;
+		}
+		else{
+			return FALSE;
+		}
+	}
+
+	function resetPassword(){
+		$Password = password_hash($this->input->post('Password'), PASSWORD_DEFAULT);
+		$id = $this->session->userdata('Id');
+		
+		$this->db->trans_start();
+		$data = array(
+			'Password' => $Password
+		);
+		$this->db->where('id', $id);
+		$this->db->update('patient', $data);
+		$afftectedRows=$this->db->affected_rows();
+		$this->db->trans_complete();
+		if($afftectedRows>0){
+			return TRUE;
+		}
+		else{
+			return FALSE;
+		}
+	}
+
+
+	// public function signup(){
+	// 	$salt = $this->salt();
+	// 	$otpkey = mt_rand(1000,9999); 
+	// 	$password = $this->makePassword($this->input->post('Password'), $salt);
+	// 	$email = $this->input->post('Email');
+		
+	// 	$this->db->trans_start();
+		
+	// 	$member   = array(
+    //         'UserName'    	 => $this->input->post('UserName'),
+	// 		'Password'     	 => $password,
+	// 		'HospitalUnitId' => 1,
+	// 		'RecordStatus'   => 1,
+	// 		'CreatedDate'	 => date('Y-m-d H:i:s',now()),
+	// 		'CreatedBy'		 => 1
+	// 	);
+	// 	$this->db->insert('SysMembership', $member);
+	// 	$MembershipId = $this->db->insert_id();
+		
+	// 	$patient = array(
+	// 		'FirstName'     => $this->input->post('FirstName'),
+	// 		'LastName'		=> $this->input->post('LastName'),
+	// 		'Password'     	=> $password,
+	// 		'GenderId'		=> $this->input->post('GenderId'),
+	// 		'DateOfBirth'   => $this->input->post('DateOfBirth'),
+	// 		'MobilePhone'	=> $this->input->post('MobilePhone'),
+	// 		'Email'			=> $this->input->post('Email'),
+	// 		'RecordStatus'  => 1,
+	// 		'CreatedDate'	=> date('Y-m-d H:i:s',now()),
+	// 		'CreatedBy'		=> 1
+	// 	);
+	// 	$this->db->insert('Patient', $patient);
+	// 	$PatientId = $this->db->insert_id();
+
+	// 	$otp = array(
+	// 		'PatientId'		=> $PatientId,
+	// 		'MembershipId'	=> $MembershipId,
+	// 		'OTP'			=> $otpkey,
+	// 		'ExpiredTime'	=> date("Y-m-d H:i:s",strtotime(date("Y-m-d H:i:s")." +2 minutes")),
+	// 		'RecordStatus'  => 1,
+	// 		'CreatedDate'	=> date('Y-m-d H:i:s',now()),
+	// 		'CreatedBy'		=> 1
+	// 	);
+	// 	$this->db->insert('OTP', $otp);
+
+	// 	$this->mail_otp($otpkey,$email);
+
+	// 	$afftectedRows=$this->db->affected_rows();
+    //     $this->db->trans_complete();
+    //     if($afftectedRows>0){
+    //         return TRUE;
+    //     }
+    //     else{
+    //         return FALSE;
+    //     }
+	// }
 }
